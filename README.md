89535008fd6a0c070b8f237404d2e23ec89b58be
Mengsreang-Chhoeung <chhoeungmengsreang789@gmail.com>
2023-02-04 18:15:35 +0700

    initialize project

---
+++ README.md
@@ -0,0 +1,3 @@
+# C Program App
+
+Anything here...
---
+++ exercises/general/2.escape_sequence.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+	// using \t for tab and \n for new line
+	printf("Hello\tWorld\n");
+	// using "" and ''
+	printf("What\'s your name? My name is \"Kok\".");
+	// using \a for have sound alarm or beep
+	printf("\nHello World\a");
+	// using \b for move cursor from right to left
+	printf("\nHello World\b\b\b");
+	getch();
+	return 0;
+}
